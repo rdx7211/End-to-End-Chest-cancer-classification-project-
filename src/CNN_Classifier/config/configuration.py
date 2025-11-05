04d574eab1bfa90a9fbc889f4f3cc11224c7405c
rdx7211 <bulan4321studypurpose@gmail.com>
2025-11-05 12:03:20 +0530

    pipeline connected with mlfow

--- src/CNN_Classifier/config/configuration.py
+++ src/CNN_Classifier/config/configuration.py
@@ -1,7 +1,7 @@
 import os
 from CNN_Classifier.constants import *
-from CNN_Classifier.utils.common import read_yaml, create_directories
-from CNN_Classifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,TrainingConfig)
+from CNN_Classifier.utils.common import read_yaml, create_directories, save_json
+from CNN_Classifier.entity.config_entity import (DataIngestionConfig, EvaluationConfig,PrepareBaseModelConfig,TrainingConfig,EvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -65,6 +65,18 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
+            mlflow_uri="https://dagshub.com/rdx7211/End-to-End-Chest-cancer-classification-project-.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
+
     
 
 
